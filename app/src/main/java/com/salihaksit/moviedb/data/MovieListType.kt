450a3a4a5b93f1f23ba6bb246fbe20fac385e2e2
salih <salih.aksit@portalgrup.com>
2019-10-19 19:07:05 +0300

    add : new cluster as a movieListType

--- app/src/main/java/com/salihaksit/moviedb/data/MovieListType.kt
+++ app/src/main/java/com/salihaksit/moviedb/data/MovieListType.kt
@@ -1,7 +1,18 @@
 package com.salihaksit.moviedb.data
 
+
+/*
+    WARNING:
+    [enum].values() always return the same order of elements,
+    that's why we manually keep this order.
+    E.g -> Let's say you want "Popular" comes first, you have to change :
+        - places of TopRated and Popular
+        - order parameter
+ */
+
 enum class MovieListType(val header: String, val sortType: String, val order: Int) {
     TopRated("Top Rated", "vote_count.desc", 0),
     Popular("Popular", "popularity.desc", 1),
     Revenue("Revenue", "revenue.desc", 2),
+    Title("Original Title", "original_title.desc", 3),
 }
