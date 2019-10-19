19f5b44ae23e146bc8c573f18208c4e221cd14cd
salih <salih.aksit@portalgrup.com>
2019-10-19 22:15:18 +0300

    add : circular progress drawable as glide placeholder

--- app/src/main/java/com/salihaksit/moviedb/ui/bindingadapters/ImageBindingAdapter.kt
+++ app/src/main/java/com/salihaksit/moviedb/ui/bindingadapters/ImageBindingAdapter.kt
@@ -3,9 +3,15 @@ package com.salihaksit.moviedb.ui.bindingadapters
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.salihaksit.moviedb.utils.getCircularProgressDrawable
 
 
 @BindingAdapter("imageUrl")
-fun loadImageUrl(view: AppCompatImageView, url: String?) {
-    Glide.with(view.context).load(url ?: "").into(view)
+fun loadImageUrl(
+    view: AppCompatImageView, url: String?
+) {
+    Glide.with(view.context)
+        .load(url ?: "")
+        .placeholder(view.context.getCircularProgressDrawable())
+        .into(view)
 }
--- app/src/main/java/com/salihaksit/moviedb/utils/CommonExtensions.kt
+++ app/src/main/java/com/salihaksit/moviedb/utils/CommonExtensions.kt
@@ -1,9 +1,19 @@
 package com.salihaksit.moviedb.utils
 
+import android.content.Context
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 
 fun <T> LifecycleOwner.observe(liveData: LiveData<T?>, lambda: (T) -> Unit) {
     liveData.observe(this, Observer { if (it != null) lambda(it) })
+}
+
+fun Context.getCircularProgressDrawable(): CircularProgressDrawable {
+    val circularProgressDrawable = CircularProgressDrawable(this)
+    circularProgressDrawable.strokeWidth = 5f
+    circularProgressDrawable.centerRadius = 30f
+    circularProgressDrawable.start()
+    return circularProgressDrawable
 }
