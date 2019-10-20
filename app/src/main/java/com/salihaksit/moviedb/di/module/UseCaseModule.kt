b5cbc650399f0748f938514107f7c16b0d6b653f
salih <salih.aksit@portalgrup.com>
2019-10-20 11:32:27 +0300

    fix : implement MovieUseCase to UseCaseModule

--- app/src/main/java/com/salihaksit/moviedb/di/module/UseCaseModule.kt
+++ app/src/main/java/com/salihaksit/moviedb/di/module/UseCaseModule.kt
@@ -1,8 +1,18 @@
 package com.salihaksit.moviedb.di.module
 
+import com.salihaksit.moviedb.data.repository.MovieRepository
+import com.salihaksit.moviedb.usecase.MovieUseCase
 import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
 
 @Module(includes = [RepositoryModule::class])
 class UseCaseModule {
+
+    @Provides
+    @Singleton
+    fun provideMovieUseCase(movieRepository: MovieRepository): MovieUseCase {
+        return MovieUseCase(movieRepository)
+    }
 
 }
